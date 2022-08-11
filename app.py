043e925e5046418e1d05e69e52b5ded555d603f9
soferreira <soferreira@microsoft.com>
2022-08-11 13:51:47 +0200

    update index html

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
 
 @app.route("/item", methods=['GET', 'POST', 'PUT', 'DELETE'])
